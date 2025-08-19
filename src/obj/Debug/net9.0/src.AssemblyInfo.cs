0c7f2bfdca51a50ac59d08d4a7a22689e2d3aedc
TRAINEE-SUMAYLO <TRAINEE-SUMAYLO@FGT-08>
2025-08-19 16:36:26 +0800

    Finished class activity

--- src/obj/Debug/net9.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net9.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93188d7cc65c5dc540c4d141d53ef1e462bf369a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7826ae06c209d58520aff894066b4e959ca77fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
